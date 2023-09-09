48b97cbd75a1c52009505f9a0d78f1ca8b5e7959
Ashish911 <ashishdongol32@gmail.com>
2023-09-09 17:08:09 +0545

    Implementing types

---
+++ types/types.ts
@@ -0,0 +1,26 @@
+export interface CarProps {
+  city_map: string;
+  class: string;
+  combination_app: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+export interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+}
+
+export interface homeProps {
+  searchParams: FilterProps;
+}
